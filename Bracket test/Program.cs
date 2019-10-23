f04bfafbd9bafee821ec419a54986d98e427c230
Brett Johnson <br.onson@outlook.com>
2019-10-23 11:00:09 -0500

    Init

---
+++ Bracket test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bracket_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass tc = new TestClass();
+            tc.FirstName = "Bob";
+            tc.LastName = "Bobson";
+
+
+            Dictionary<string, string> names = new Dictionary<string, string>();
+            names.Add("FirstName", "Brett");
+            names.Add("LastName", "Johnson");
+
+            foreach(string property in names.Keys)
+            {
+                var value = names[property];
+                tc[property] = value;
+            }
+
+            Console.WriteLine(tc["FirstName"] + " " + tc["LastName"]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Bracket test/TestClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bracket_test
+{
+    class TestClass
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string this[string propName]
+        {
+            get
+            {
+                switch(propName)
+                {
+                    case "FirstName":
+                        return this.FirstName;
+                    case "LastName":
+                        return this.LastName;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            set
+            {
+                switch (propName)
+                {
+                    case "FirstName":
+                        this.FirstName = value;
+                        break;
+                    case "LastName":
+                        this.LastName = value;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+
+
+
+
+
+    }
+}
